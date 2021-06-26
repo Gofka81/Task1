4e1fc837f57a72dbae84defa3f4ca4b776b00fda
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-06-26 17:46:39 +0300

    Complete Part3

--- src/main/java/com/epam/rd/java/basic/practice1/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Demo.java
@@ -4,7 +4,14 @@ package com.epam.rd.java.basic.practice1;
 public class Demo {
 
     public static void main(String[] args) {
-        
+        Part1.main(new String[]{});
+        System.out.print("\n\n");
+
+        Part2.main(new String[] {"10", "2", "3"});
+        System.out.print("\n\n");
+
+        Part3.main(new String[]{"10","3","11"});
+        System.out.print("\n\n");
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,7 +3,11 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
-        
+        for(String arg: args){
+            if(!arg.equals(args[0]))
+                System.out.print(" ");
+            System.out.print(arg);
+        }
     }
 	
 }
