e38f77e3250fa364e565fae26df97a887bda27c3
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-06-26 21:20:14 +0300

    fixed Part4

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -5,9 +5,9 @@ public class Part4 {
     public static void main(String[] args) {
         int num1 = Integer.parseInt(args[0]);
         int num2 = Integer.parseInt(args[1]);
-        System.out.print(N_S_D(num1,num2));
+        System.out.print(nsd(num1,num2));
     }
-    public static int N_S_D(int a, int b){
+    public static int nsd(int a, int b){
         while (b != 0){
             if(a == 0)
                 return b;
